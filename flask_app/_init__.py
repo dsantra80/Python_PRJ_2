3bf33a3ccace8d9a65c6acf9d1b7955dfeb1a99d
dsantra80 <60920557+dsantra80@users.noreply.github.com>
2024-05-27 04:35:35 +0530

    Updated _init__.py

--- flask_app/_init__.py
+++ flask_app/_init__.py
@@ -2,11 +2,10 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-
-    # Load configuration from config.py
-    app.config.from_object('config.Config')
-
-    # Register blueprints
+    
+    # Correct import path for config
+    app.config.from_object('flask_app.config.Config')
+    
     from .routes import routes
     app.register_blueprint(routes)
 
